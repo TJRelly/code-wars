38ca68715363997b8a1baf198908a805dd182b1e
TJ <xxterrelljacksonxx@gmail.com>
2023-03-06 08:38:49 -0500

    solutions

---
+++ 6 kyu/Playing with digits.js
@@ -0,0 +1,15 @@
+//Solution 1
+function digPow(n, p){
+    let nums = n.toString().split('')
+    let pwrPlay = nums.map((num, i) => num**(p + i))
+    let result = pwrPlay.reduce((acc, num) => acc + num)
+    let k = result / n
+    return !(result % n) ? k : -1
+  }
+
+  //Revised
+  function digPow(n, p){
+    let nums = n.toString().split('')
+    let digPlay = nums.reduce((acc, num, i) => acc + num ** (p + i), 0)
+    return digPlay % n ? -1 : digPlay / n
+  }
